c1020cdd1da555c71d0d067d0d12e0715fbaa88c
Vitaliy <vostrikov.vitaliy.mikhailovich@gmail.com>
2022-01-26 12:31:29 +0300

    Fix export entities.

--- lib/calendar_ui.dart
+++ lib/calendar_ui.dart
@@ -1,5 +1,6 @@
 export 'bloc/calendar_bloc.dart';
-export 'package:calendar/calendar.dart' show Day, Month, Year;
+export 'package:calendar/calendar.dart'
+    show DayInterface, MonthInterface, YearInterface;
 
 export 'calendar_month_ui/calendar_month_ui.dart';
 export 'calendar_week_ui/calendar_week_ui.dart';
