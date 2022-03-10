0c90e8b55c309617e89a7ab523d280f3a51885ea
Vitaliy <vostrikov.vitaliy.mikhailovich@gmail.com>
2022-03-10 14:17:38 +0300

    Export week interface.

--- lib/calendar_ui.dart
+++ lib/calendar_ui.dart
@@ -1,6 +1,6 @@
 export 'bloc/calendar_bloc.dart';
 export 'package:calendar/calendar.dart'
-    show DayInterface, MonthInterface, YearInterface;
+    show DayInterface, MonthInterface, YearInterface, WeekInterface;
 
 export 'calendar_month_ui/calendar_month_ui.dart';
 export 'calendar_week_ui/calendar_week_ui.dart';
